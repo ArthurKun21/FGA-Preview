495bb402536ed41ddc2cf374458ac4a5e5f2743d
arthur <16458204+ArthurKun021@users.noreply.github.com>
2024-01-18 01:58:56 +0800

    change into data object

--- scripts/src/main/java/io/github/fate_grand_automata/scripts/models/EnemyTarget.kt
+++ scripts/src/main/java/io/github/fate_grand_automata/scripts/models/EnemyTarget.kt
@@ -1,9 +1,9 @@
 package io.github.fate_grand_automata.scripts.models
 
 sealed class EnemyTarget(val autoSkillCode: Char) {
-    object A : EnemyTarget('1')
-    object B : EnemyTarget('2')
-    object C : EnemyTarget('3')
+    data object A : EnemyTarget('1')
+    data object B : EnemyTarget('2')
+    data object C : EnemyTarget('3')
 
     companion object {
         val list by lazy { listOf(A, B, C) }
