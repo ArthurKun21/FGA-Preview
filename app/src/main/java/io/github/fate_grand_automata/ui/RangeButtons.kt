44ce3b4a9f8c9ca40e74589d8a173afaf69bec70
ArthurKun <16458204+ArthurKun21@users.noreply.github.com>
2024-03-27 11:44:49 +0800

    Updated the Skills and Append Launcher UI (#34)
    - Converted the Stepper into Multiple Buttons
    - Simplify the all upgrade

---
+++ app/src/main/java/io/github/fate_grand_automata/ui/RangeButtons.kt
@@ -0,0 +1,86 @@
+package io.github.fate_grand_automata.ui
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.LocalTextStyle
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+
+
+@Composable
+fun RangeButtons(
+    modifier: Modifier = Modifier,
+    value: Int,
+    onValueChange: (Int) -> Unit,
+    triState: Boolean = false,
+    valueRange: IntRange,
+    enabled: Boolean = true,
+    textStyle: TextStyle = LocalTextStyle.current,
+    shape: Shape = ButtonDefaults.shape,
+    valueRepresentation: (Int) -> String = { it.toString() }
+) {
+    var currentValue by remember(value) { mutableIntStateOf(value) }
+
+    FlowRow(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center
+    ) {
+        for (i in valueRange) {
+            Button(
+                onClick = {
+                    if (i == currentValue && triState) {
+                        currentValue = valueRange.first - 1
+                        onValueChange(valueRange.first - 1)
+                    } else {
+                        currentValue = i.coerceIn(valueRange)
+                        onValueChange(i.coerceIn(valueRange))
+                    }
+                },
+                enabled = enabled,
+                border = BorderStroke(
+                    width = when(currentValue == i){
+                        true -> 2.dp
+                        false -> 1.dp
+                    },
+                    color = when(currentValue == i){
+                        true -> MaterialTheme.colorScheme.onSurface
+                        false -> MaterialTheme.colorScheme.onSecondary
+                    },
+                ),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = when(currentValue == i){
+                        true -> MaterialTheme.colorScheme.secondary
+                        false -> MaterialTheme.colorScheme.surface
+                    },
+                    contentColor = when(currentValue == i){
+                        true -> MaterialTheme.colorScheme.onSecondary
+                        false -> MaterialTheme.colorScheme.onSurface
+                    }
+                ),
+                shape = shape,
+                modifier = Modifier.padding(horizontal = 2.dp)
+            ) {
+                Text(
+                    valueRepresentation(i),
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier,
+                    style = textStyle
+                )
+            }
+        }
+    }
+}
